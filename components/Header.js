0b9c82bf88bbe67f6f75e832c2801cdbce3dd026
Kikemuncher <Stapelton@discreet.vip>
2025-03-07 00:13:13 +0200

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -6,12 +6,12 @@ const Header = () => {
   const router = useRouter();
 
   return (
-    <div className="fixed top-4 left-1/2 transform -translate-x-1/2 z-50">
-      {/* ✅ Centered Logo - Does NOT interfere with scrolling */}
+    <div className="absolute top-0 left-0 w-full flex justify-center z-50 pointer-events-none">
+      {/* ✅ Logo - Small padding at the top, centered, does NOT affect scrolling */}
       <motion.img
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
-        className="w-32 h-auto cursor-pointer"
+        className="w-28 h-auto cursor-pointer mt-2 pointer-events-auto"
         src="/logo.png"
         alt="New Logo"
         onClick={() => router.push("/")}
