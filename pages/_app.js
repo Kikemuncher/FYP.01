b52973a07d5c777fa16584363e8b6bc4001bcc44
Kikemuncher <Stapelton@discreet.vip>
2025-03-12 16:55:35 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,7 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-body {
-  background-color: red !important;
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
