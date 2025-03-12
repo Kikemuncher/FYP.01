eea19ced0006f88683748af309a9ac23f66af8a4
Kikemuncher <Stapelton@discreet.vip>
2025-03-12 18:14:54 +0200

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -6,18 +6,15 @@ const Header = () => {
   const router = useRouter();
 
   return (
-    <div className="fixed top-0 left-0 w-full flex justify-center z-50 bg-teal-700 py-3 shadow-md">
-      {/* ✅ Centered Logo - Properly aligned */}
-      <motion.img
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 0.5 }}
-        className="w-28 h-auto cursor-pointer"
-        src="/logo.png"
-        alt="New Logo"
-        onClick={() => router.push("/")}
-      />
-    </div>
+    <motion.img
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5 }}
+      className="fixed top-4 left-1/2 transform -translate-x-1/2 z-50 w-28 h-auto cursor-pointer"
+      src="/logo.png"
+      alt="New Logo"
+      onClick={() => router.push("/")}
+    />
   );
 };
 
