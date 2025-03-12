cfc20d15cf92ecabbf27b7b4f7c823126d2dba00
Kikemuncher <Stapelton@discreet.vip>
2025-03-12 18:28:30 +0200

    Create LogoOnly.js

---
+++ components/LogoOnly.js
@@ -0,0 +1,22 @@
+// components/LogoOnly.js
+import React from "react";
+import { useRouter } from "next/router";
+import { motion } from "framer-motion";
+
+const LogoOnly = () => {
+  const router = useRouter();
+
+  return (
+    <motion.img
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5 }}
+      className="fixed top-4 left-1/2 transform -translate-x-1/2 z-50 w-28 h-auto cursor-pointer"
+      src="/logo.png"
+      alt="New Logo"
+      onClick={() => router.push("/")}
+    />
+  );
+};
+
+export default LogoOnly;
