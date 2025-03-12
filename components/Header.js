25382c5fd05d1ca3b0b9ba813bc6f5191f37fc57
Kikemuncher <Stapelton@discreet.vip>
2025-03-12 19:07:36 +0200

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -6,15 +6,19 @@ const Header = () => {
   const router = useRouter();
 
   return (
-    <motion.img
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 0.5 }}
-      className="fixed top-4 left-1/2 transform -translate-x-1/2 z-50 w-28 h-auto cursor-pointer"
-      src="/logo.png"
-      alt="New Logo"
-      onClick={() => router.push("/")}
-    />
+    <>
+      {/* Logo only - the container div is set to "invisible" not "hidden" */}
+      <motion.img
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.5 }}
+        className="fixed top-4 left-1/2 transform -translate-x-1/2 z-[9999] w-28 h-auto cursor-pointer"
+        src="/logo.png"
+        alt="New Logo"
+        onClick={() => router.push("/")}
+        style={{ display: "block" }}
+      />
+    </>
   );
 };
 
